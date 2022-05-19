a99dfac5851018b6d0459895234f9dccda668c2e
Amine <toshamine@gmail.com>
2022-05-19 15:19:44 +0100

    Final Commit + Test

--- src/main/java/com/example/challenge/Controllers/ChallengeController.java
+++ src/main/java/com/example/challenge/Controllers/ChallengeController.java
@@ -4,6 +4,7 @@ package com.example.challenge.Controllers;
 import com.example.challenge.Models.Talk;
 import com.example.challenge.Services.ChallengeService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,11 +18,11 @@ public class ChallengeController {
 
     private ChallengeService challengeService;
 
-    @PostMapping("/duration")
-    public List<String> duration(@RequestBody List<String> tracks)
+    @PostMapping("/arrange")
+    public List<String> arrange(@RequestBody List<String> tracks)
     {
         List<Talk> talks= challengeService.duration(tracks);
-        return challengeService.morningtrack(talks);
+        return challengeService.arrangetracks(talks,1);
     }
 
 }
